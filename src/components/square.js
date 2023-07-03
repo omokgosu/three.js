39b7ac4b67ead568502c3865d9529aa3f9e31bd1
양진성 <anfakt0606@naver.com>
2023-07-03 17:15:41 +0900

    basic square

--- src/components/square.js
+++ src/components/square.js
@@ -1,8 +1,25 @@
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls , PerspectiveCamera } from '@react-three/drei'
+
+import styled from "styled-components";
+
 export default function Square(){
 
     return(
-        <section className="square">
-            <h2>스퀘어</h2>
-        </section>
+        <SquareLayout>
+             <Canvas shadows>
+                <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.4} />
+                <PerspectiveCamera makeDefault fov={50} position={[3,2,5]} />
+
+                <mesh>
+                    <boxGeometry args={[1,1,1]} />
+                    <meshBasicMaterial color={'red'} />
+                </mesh>
+            </Canvas>
+        </SquareLayout>
     )
 }
+
+const SquareLayout = styled.section`
+    height: calc(100vh - 43px);
+`
