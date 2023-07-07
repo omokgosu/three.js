31ed333d680ed0bbb7f90aa2a5992c6b3262b105
양진성 <anfakt0606@naver.com>
2023-07-07 21:46:16 +0900

    자동차 3D 애니메이션

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Suspense} from 'react';
 import { Canvas } from '@react-three/fiber'
 import './style.css'
-import { OrbitControls, PerspectiveCamera} from '@react-three/drei'
+import { CubeCamera, Environment, OrbitControls, PerspectiveCamera} from '@react-three/drei'
+import Ground  from './Ground';
+import CarInfo from './CarInfo';
+import { Rings } from './Rings';
+import Boxes from './Boxes';
+import {Bloom , ChromaticAberration, EffectComposer} from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing'
+import { FloathingGrid } from './FloatingGrid';
 
 function CarShow(){
   return (
@@ -9,10 +16,59 @@ function CarShow(){
       <OrbitControls target={[0, 0.35 ,0]} maxPolarAngle={1.45} />
     
       <PerspectiveCamera makeDefault fov={50} position={[3,2,5]}/>
-      <mesh>
-        <boxGeometry args={[1,1,1]}/>
-        <meshBasicMaterial color={"red"} />
-      </mesh>
+      
+      <color args={[0,0,0]} attach="background"/>
+
+      {/* <Car /> */}
+      <CubeCamera resolution={256} frames={Infinity}>
+        {
+          (texture) => (
+            <>
+              <Environment map={texture} />
+              <CarInfo />
+            </>
+          )
+        }
+      </CubeCamera>
+      <Rings />
+      <Boxes />
+      <FloathingGrid />
+
+      <spotLight 
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5,5,0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight 
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5,5,0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground />
+      <EffectComposer>
+          <Bloom 
+            blendFunction={BlendFunction.ADD}
+            intensity={1.3}
+            width={300}
+            height={300}
+            kernelSize={5}
+            luminaceThreshold={0.15}
+            luminaceSmoothing={0.025}
+          />
+          <ChromaticAberration 
+            blendFunction={BlendFunction.NORMAL}
+            offset={[0.0005,0.0012]}
+          />
+      </EffectComposer>
     </>
   );
 }
